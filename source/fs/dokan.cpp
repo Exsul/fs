ac652db97ff66e215bca6937f24e8bb96a6617c3
Kirill Berezin <enelar@develop-project.ru>
2013-11-24 04:18:47 +0400

    Refactored empty device

--- source/fs/dokan.cpp
+++ source/fs/dokan.cpp
@@ -14,15 +14,25 @@ void dokan::InitDokan( std::wstring mount_point )
 
   oper = NEW DOKAN_OPERATIONS;
   opt = NEW DOKAN_OPTIONS;
+
+  memset(oper, 0, sizeof(DOKAN_OPERATIONS));
   memset(opt, 0, sizeof(DOKAN_OPTIONS));
+
   opt->GlobalContext = reinterpret_cast<ULONG64>(NEW dokan_proxy());
   opt->ThreadCount = 1;
   opt->Version = DOKAN_VERSION;
   opt->ThreadCount = 0; // use default
   opt->Options |= DOKAN_OPTION_DEBUG | DOKAN_OPTION_KEEP_ALIVE;
 
-  //ax::StrFastCopy<wchar_t>(opt->MountPoint, mount_point.c_str());
-  memset(oper, 0, sizeof(DOKAN_OPERATIONS));
+
+  auto CopyMountPoint = [&]( const std::wstring &source)
+  {
+    wchar_t *dest = nullptr;
+    ax::StrFastCopy(dest, source.c_str());
+    opt->MountPoint = dest;
+  };
+
+  CopyMountPoint(mount_point);
   InitMethods(oper);
 }
 
